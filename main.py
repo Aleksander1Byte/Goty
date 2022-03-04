f5be65b763c4dd61bbd1e819009ea4882c177198
Aleksandr <aleksandr.gerasimov.05@gmail.com>
2022-03-04 16:35:14 +0700

    Basic init

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-from flask import Flask, Response
+from flask import Flask, Response, render_template
+from flask_login import login_user, login_required, logout_user, current_user, \
+    LoginManager
 
+from data.db_session import create_session
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return
+    #  db_sess = create_session()
+    #  return db_sess.query().get(user_id)
 
 
 def main():
@@ -10,7 +23,8 @@ def main():
 
 @app.route('/')
 def page():
-    return 'Текст'
+    return render_template('index.html', title='Goty',
+                           current_user=current_user)
 
 
 if __name__ == '__main__':
