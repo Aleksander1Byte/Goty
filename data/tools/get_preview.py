250b387420a5b5a6344ea6aab43bea8069d3dc37
Aleksandr <aleksandr.gerasimov.05@gmail.com>
2022-03-06 19:13:12 +0700

    Fixed bug with random preview

--- data/tools/get_preview.py
+++ data/tools/get_preview.py
@@ -1,5 +1,5 @@
 import cv2
-from random import randint
+from random import randrange
 
 
 def get_preview(video_file) -> str:
@@ -7,11 +7,11 @@ def get_preview(video_file) -> str:
     cap = cv2.VideoCapture(video_file)
     frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     fps = cap.get(cv2.CAP_PROP_FPS)
-    msecs = int(frames * fps)
-    cap.set(cv2.CAP_PROP_POS_MSEC, randint(2000, msecs - 1000))  # проблема где-то тут
+    msecs = int((frames * fps) // 4)
+    cap.set(cv2.CAP_PROP_POS_MSEC, randrange(1000, msecs - 1000, 1000))
     success, image = cap.read()
     if success:
-        name = f'{randint(1, msecs)}.jpg'
+        name = f'{randrange(1000, msecs - 1000, 1000)}.jpg'
         cv2.imwrite(name, image)
         cv2.waitKey()
         return name
